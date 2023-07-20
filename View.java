d8f6b167b010ac033402a932fc50a7815087e314
zero-jedynkowy <01110010pl@gmail.com>
2023-07-20 15:33:06 +0200

    Light rebuilding of the window's top layout

--- View.java
+++ View.java
@@ -12,6 +12,7 @@ public class View extends JPanel
     JButton selectFolderButton;
 
     JLabel selectFolderPathTitleLabel;
+    JLabel selectFolderPathUndertitleLabel;
     JScrollPane scrollSelectFolderPath;
     JLabel selectFolderPathLabel;
 
@@ -22,6 +23,7 @@ public class View extends JPanel
         this.setLayout(null);
         
         //SET VIEW METHODS
+        this.setSelectFolderTitle();
         this.setSelectButton();
         this.setScrollSelectFolderPath();
 
@@ -30,25 +32,35 @@ public class View extends JPanel
 
 
     //SET VIEW METHODS
+    public void setSelectFolderTitle()
+    {
+        this.selectFolderPathTitleLabel = new JLabel("Wybierz folder do zrobienia listy:");
+        this.add(this.selectFolderPathTitleLabel);
+        this.selectFolderPathTitleLabel.setBounds(15, 15, 200, 15);
+        this.selectFolderPathTitleLabel.setVisible(true);
+    }
+
     public void setSelectButton()
     {
         this.selectFolderButton = new JButton("Wybierz folder");
-        this.selectFolderButton.setLocation(15, 15);
+        this.selectFolderButton.setLocation(15, 50);
         this.selectFolderButton.setSize(this.selectFolderButton.getText().length()*9, 50);
         this.add(this.selectFolderButton);
     }
 
     public void setScrollSelectFolderPath()
     {
+        this.selectFolderPathUndertitleLabel = new JLabel("Ścieżka wybranego folderu:");
+        this.add(this.selectFolderPathUndertitleLabel);
+        this.selectFolderPathUndertitleLabel.setBounds(this.selectFolderButton.getWidth() + this.selectFolderButton.getX() + 15, this.selectFolderButton.getY(), 200, 15);
+        
         this.selectFolderPathLabel = new JLabel("   BRAK");
         this.scrollSelectFolderPath = new JScrollPane(this.selectFolderPathLabel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        this.selectFolderPathLabel.setBounds(25, 0, this.scrollSelectFolderPath.getWidth() + 200, this.scrollSelectFolderPath.getHeight());
-        this.scrollSelectFolderPath.setBounds(this.selectFolderButton.getWidth() + this.selectFolderButton.getX() + 15, 30, this.getWidth() - this.selectFolderButton.getWidth() - this.selectFolderButton.getX() - 45, 50);
+        this.scrollSelectFolderPath.setBounds(this.selectFolderPathUndertitleLabel.getX(), this.selectFolderPathUndertitleLabel.getHeight() + this.selectFolderPathUndertitleLabel.getY() + 5, 50, 30);
+        this.scrollSelectFolderPath.setSize(this.getWidth() - this.scrollSelectFolderPath.getX() - 25, 40);
+        this.selectFolderPathLabel.setBounds(0, 0, this.scrollSelectFolderPath.getWidth() + 200, this.scrollSelectFolderPath.getHeight());
         this.selectFolderPathLabel.setVisible(true);
         this.add(this.scrollSelectFolderPath);
-        this.selectFolderPathTitleLabel = new JLabel("Ścieżka wybranego folderu:");
-        this.add(this.selectFolderPathTitleLabel);
-        this.selectFolderPathTitleLabel.setBounds(this.scrollSelectFolderPath.getX(), this.scrollSelectFolderPath.getY() - 35, 200, 50);
     }
 
     public void updateSelectFolderPath(File newPath)
