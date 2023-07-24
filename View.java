65319128a3897c4c7214fd05fad3c9066a0fe9d8
zero-jedynkowy <01110010pl@gmail.com>
2023-07-24 21:30:57 +0200

    Add the first label to creating list mode's panel

--- View.java
+++ View.java
@@ -1,11 +1,15 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.GridLayout;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.SwingConstants;
 
 public class View extends JPanel
 {
@@ -14,6 +18,11 @@ public class View extends JPanel
     JPanel createListPanel;
     JPanel readListPanel;
 
+    //CREATE LIST PANEL'S ELEMENTS
+    JLabel chooseFolderLabel;
+    JButton chooseFolderButton;
+    JLabel chooseFolderPathLabel;
+
     public View()
     {
         super();
@@ -21,6 +30,7 @@ public class View extends JPanel
         
         //SET ELEMENTS' APPEARANCE
         this.setTabbedModes();
+        this.setCreateListModePanel();
 
         this.setVisible(true);
     }
@@ -35,6 +45,20 @@ public class View extends JPanel
         this.add(this.tabbedModes, BorderLayout.CENTER);
         this.changeFontSize(this.tabbedModes, 15);
         this.tabbedModes.setVisible(true);
+    }
+
+    public void setCreateListModePanel()
+    {
+        this.chooseFolderLabel = new JLabel("Wybierz folder do zrobienia listy:", JLabel.CENTER);
+        this.chooseFolderLabel.setVisible(true);
+        this.chooseFolderLabel.setAlignmentX(CENTER_ALIGNMENT);
+        this.changeFontSize(this.chooseFolderLabel, 20);
+        this.chooseFolderLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+
+
+        this.createListPanel.add(Box.createVerticalStrut(10));
+        this.createListPanel.add(this.chooseFolderLabel);
     }
 
     public JPanel createNewPanel(String text)
