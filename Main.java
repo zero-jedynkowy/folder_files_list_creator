3e234886a4761d323bdbc71d6fa7b9f07d11ad59
zeroJedynkowy <01110010pl@gmail.com>
2024-02-27 19:43:34 +0100

    Add a settings file and the first step of it in the code

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import org.json.*;
 
 public class Main extends JFrame
 {
@@ -11,11 +18,12 @@ public class Main extends JFrame
     
     final static int WIDTH = 500;
     final static int HEIGHT = 500;
-
+    Map<String, Integer> settings;
+    
     public Main()
     {
         super();
-        
+        this.readSettings();
         this.setTitle("Lista plików");
         this.setLayout(new BorderLayout());
         this.setSize(WIDTH, HEIGHT);
@@ -24,7 +32,7 @@ public class Main extends JFrame
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        this.setSystemLook();
+        this.setSystemLook(this.settings.get("style"));
         this.view = new View();
         this.add(this.view, BorderLayout.CENTER);
         
@@ -45,16 +53,44 @@ public class Main extends JFrame
         return x;
     }
 
-    public void setSystemLook()
+    public void setSystemLook(int mode)
     {
+        if(mode == 1)
+        {
+            try 
+            {
+                UIManager.setLookAndFeel(
+                UIManager.getSystemLookAndFeelClassName());
+            } 
+            catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) 
+            {
+                JOptionPane.showMessageDialog(this,"Nie można załadować motywu systemu","Błąd!", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    public void readSettings()
+    {
+        String data = "";
         try 
         {
-            UIManager.setLookAndFeel(
-            UIManager.getSystemLookAndFeelClassName());
-        } 
-        catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) 
-        {
-            JOptionPane.showMessageDialog(this,"Nie można załadować motywu systemu","Błąd!", JOptionPane.ERROR_MESSAGE);
+            File myObj = new File("settings.json");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+            data += myReader.nextLine();
         }
+        myReader.close();
+        } 
+        catch (FileNotFoundException e) 
+        {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        this.settings = new HashMap<String, Integer>();
+        this.settings.put("style", 0);
+        this.settings.put("language", 0);
+        JSONObject temp = new JSONObject(data);
+        this.settings.put("style", temp.getInt("style"));
+        this.settings.put("language", temp.getInt("language"));
     }
 }
